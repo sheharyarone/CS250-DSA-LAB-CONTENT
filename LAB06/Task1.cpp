9951af188a8ef944bf70027fc5b7953013d9cf46
Sheharyar <sheharyarone@gmail.com>
2022-10-18 15:00:02 +0500

    task1

--- LAB06/Task1.cpp
+++ LAB06/Task1.cpp
@@ -1,14 +1,48 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-
-
-
+const int length = 5;
+class Queue
+{
+public:
+    int items[length];
+    int front, rear;
+    Queue()
+    {
+        front = 0;
+        rear = -1;
+    }
+    bool isEmpty()
+    {
+        return front == (rear+1);
+    }
+    bool isFull()
+    {
+        return (rear+1) == length;
+    }
+    void enqueue(int x)
+    {
+        rear++;
+        items[rear] = x;
+    }
+    int dequeue()
+    {
+        return items[front++];
+    }
+    int firstElement()
+    {
+        return items[front];
+    }
+    void clear()
+    {
+        rear = 0;
+        front = 0;
+    }
+};
 
 int main()
 {
-
 
     return 0;
 }
