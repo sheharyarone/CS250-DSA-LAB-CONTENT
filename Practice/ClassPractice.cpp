35a296c4653cacfa1ca5b08ef238962abab91562
Sheharyar <sheharyarone@gmail.com>
2022-10-20 00:39:54 +0500

    swapping of nodes except first in singly

--- Practice/ClassPractice.cpp
+++ Practice/ClassPractice.cpp
@@ -10,34 +10,70 @@ struct Node
     struct Node *next;
 }
     *first = NULL;
-
 bool isEmpty()
 {
-    return true;
+    return first == NULL;
 }
-void del(struct Node *p)
-{
-    if (!isEmpty())
-    {
-        int counter = 1;
 
-        while (p->next != NULL)
-        {
-            struct Node *temp;
-            temp=p;
-            p = p->next;
-            counter += 1;
-            if (counter % 2 == 0)
-            {
-                
-                delete temp;
-            }
-        }
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = new Node();
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+
+        t = new Node();
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << (p->data) << endl;
+        p = p->next;
+    }
+}
+
+void DisplayRecursive(struct Node *p)
+{
+    if (p != NULL)
+    {
+        cout << p->data << endl;
+        DisplayRecursive(p->next);
+    }
+}
+void swapping(struct Node *p)
+{
+    Node *temp = new Node();
+
+    while (p != NULL && p->next != NULL && p->next->next!=NULL)
+    {
+        temp = p->next;  // 2 IN TEMP
+        p->next=p->next->next; // next of 1 is 3
+        temp->next=temp->next->next;  // next of 2 is 4
+        
+        p->next->next=temp; // next of 3 is 2
+        p=p->next->next;
     }
 }
 
 int main()
 {
+    int A[] = {1, 2, 3, 4,5};
 
+    create(A, sizeof(A) / sizeof(int));
+    Display(first);
+    swapping(first);
+    Display(first);
     return 0;
 }
