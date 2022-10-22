11caa5e6a4158366d0d82bca4a8ba439d5f1a927
Sheharyar <sheharyarone@gmail.com>
2022-10-22 22:54:17 +0500

    Assignment1 init

---
+++ Assignments/01/List.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Dictionary.h"
+using namespace std;
+
+class ListNode{   //classs definition of Node 
+  public:
+    Dictionary data;
+    ListNode *next;
+};
+
+//PASTE YOUR LINKED LIST CODE HERE
---
+++ Assignments/01/main.cpp
@@ -0,0 +1,9 @@
+#include "Parser.h"
+
+using namespace std; 
+
+int main()
+{
+  mainMenu(); //call the main menu function from Parser.h
+  return 0;
+}
