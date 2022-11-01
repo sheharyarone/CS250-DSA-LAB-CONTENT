7aede4930a0034c4a1a792717f3b04906ec259b1
Sheharyar <sheharyarone@gmail.com>
2022-11-01 14:57:34 +0500

    task1

--- LAB08/task1.cpp
+++ LAB08/task1.cpp
@@ -1,8 +1,22 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
+
+int factorial(int num)
+{
+    cout << num << endl;
+
+    if (num != 0)
+    {
+        return num * factorial(num - 1);
+    }
+    cout<<"BREAK BCZ 0 IS THERE."<<endl;
+    return 1;
+}
+
 int main()
 {
-    
+    cout << "recursive call for : " << endl;
+    cout << "ANSWER IS : " << factorial(5);
     return 0;
 }
