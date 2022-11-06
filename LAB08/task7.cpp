ce97e41c7d73c50354f25f16fa33ce64839c9115
Sheharyar <sheharyarone@gmail.com>
2022-11-06 12:02:07 +0500

    wkring for even length

--- LAB08/task7.cpp
+++ LAB08/task7.cpp
@@ -82,29 +82,34 @@ public:
         }
         cout << endl;
     }
-    void rearrangeEvenOdd(node *ploc, node *loc)
+    void rearrangeEvenOdd(node *ploc, node *loc, node *sloc, int node_num = 1) // ploc:head loc sloc
     {
-
-        if (loc != nullptr)
+        cout<<"DATA : "<<ploc->data<<" "<<loc->data<<" "<<sloc->data<<endl;
+        if (sloc != nullptr)
         {
-            if (ploc == nullptr)
+            if (node_num % 2 != 0)
             {
-                // loc = loc->next;
-                rearrangeEvenOdd(loc, loc->next->next);
-                loc = loc->next;
-
+                // if (ploc == head)
+                // {
+                //     loc->next=nullptr;
+                //     tail=loc;
+                // }
+                
+                ploc->next = sloc;
+                cout<<"if : ";printll();
+                rearrangeEvenOdd(loc, sloc, sloc->next, ++node_num);
+                if (ploc == head)
+                {
+                    loc->next=nullptr;
+                    tail=loc;
+                }
             }
             else
             {
-                ploc->next = loc;
-                ploc = ploc->next;
-                rearrangeEvenOdd(loc, loc->next->next);
-                loc = loc->next;
-            }
-            loc->next = ploc;
-            if (ploc == nullptr)
-            {
-                tail = loc;
+                sloc = sloc->next;
+                loc->next->next = ploc;
+                cout<<"else : ";printll();
+                rearrangeEvenOdd(loc, loc->next, sloc, ++node_num);
             }
         }
     }
@@ -113,13 +118,16 @@ public:
 int main()
 {
     linkedList ll;
-    ll.insetAtHead(7);
-    ll.insetAtHead(4);
-    ll.insertAtTail(9);
-    ll.insertAtTail(12);
-    ll.insertAtTail(34);
-    ll.insertAtTail(41);
+    ll.insetAtHead(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.insertAtTail(6);
+    ll.insertAtTail(7);
+    // ll.insertAtTail(8);
+    // ll.insertAtTail(9);
     ll.printll();
-    ll.rearrangeEvenOdd(nullptr, ll.head);
+    ll.rearrangeEvenOdd(ll.head, ll.head->next, ll.head->next->next);
     ll.printll();
 }
