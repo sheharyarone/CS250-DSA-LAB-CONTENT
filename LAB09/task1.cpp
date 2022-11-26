0739f89f706b35af2bd53faafa636b657b373410
Sheharyar <sheharyarone@gmail.com>
2022-11-26 23:24:08 +0500

    selectionSort func

---
+++ LAB09/task1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printArr(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+void selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    printArr(arr, size);
+}
+
+int main()
+{
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    selectionSort(arr, 9);
+    return 0;
+}
