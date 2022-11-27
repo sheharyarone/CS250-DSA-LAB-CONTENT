5c8f0ef5fb765630f7aeb585003b35b19b866916
Sheharyar <sheharyarone@gmail.com>
2022-11-27 19:28:48 +0500

    shortBubble Sort algo

--- LAB09/task1.cpp
+++ LAB09/task1.cpp
@@ -20,13 +20,33 @@ void bubbleSort(int arr[], int size) {
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
-
         }
     }
     printArr(arr, size);
-
-
 }
+
+void shortBubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        // n-1 passes
+        bool isSwap = false;
+        cout << "YES" << endl;
+
+        for (int j = 0; j < size - i - 1; j++) {
+            // single pass which set last element to the largest
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                isSwap = true;
+            }
+        }
+        if (!isSwap) {
+            break;
+        }
+    }
+    printArr(arr, size);
+}
+
 
 void selectionSort(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
@@ -46,8 +66,9 @@ void selectionSort(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 //    selectionSort(arr, 9);
-    bubbleSort(arr, 9);
+//    bubbleSort(arr, 9);
+    shortBubbleSort(arr, 9);
     return 0;
 }
