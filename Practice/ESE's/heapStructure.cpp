af2f0648886f0d77f15befbf92d06939af84d25e
Sheharyar <sheharyarone@gmail.com>
2023-01-06 23:52:33 +0500

    practice trees

---
+++ Practice/ESE's/heapStructure.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void Insert(int *arr, int n)
+{
+}
+
+int main()
+{
+    int *arr = new int[8];
+    arr[1] = 5;
+    cout << arr[1];
+
+    return 0;
+}
